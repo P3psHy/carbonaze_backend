252b55368533cebfe3da90684478c4a9597143e3
dridri85 <adrirando@gmail.com>
2026-03-16 14:58:59 +0100

    repaired project init

--- src/main/java/fr/sdv/hackaton/carbonaze/CarbonazeApplication.java
+++ src/main/java/fr/sdv/hackaton/carbonaze/CarbonazeApplication.java
@@ -1,4 +1,4 @@
-package carbonaze.com.example.demo;
+package fr.sdv.hackaton.carbonaze;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CarbonazeApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CarbonazeApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CarbonazeApplication.class, args);
+    }
 
 }
