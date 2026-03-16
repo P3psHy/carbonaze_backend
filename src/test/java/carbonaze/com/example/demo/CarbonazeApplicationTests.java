d58e68e10d5320a21a13a41c3c3410d541cbb1e9
P3psHy <76128319+P3psHy@users.noreply.github.com>
2026-03-16 14:38:51 +0100

    ajout base

---
+++ src/test/java/carbonaze/com/example/demo/CarbonazeApplicationTests.java
@@ -0,0 +1,13 @@
+package carbonaze.com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarbonazeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
